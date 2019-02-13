bcb66e20cb4a3806c80e9d12af5fe9ce9aa2defb
david <davidcidrod@gmail.com>
2019-02-13 14:06:04 +0100

    buildsplash

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+'use strict'
 
+const main = () => {
+    
+    const buildDom = (html) => {
+
+        const main = document.querySelector("main");
+        main.innerHTML = html;
+        return main;
+    }
+    const buildSplashScreen = () => {
+        const splashScreen = buildDom(`
+        <h1>Hello</h1>`)
+
+    }
+    buildSplashScreen();
+}
+
+
+window.addEventListener('load',main);                       //callback de carga el main
