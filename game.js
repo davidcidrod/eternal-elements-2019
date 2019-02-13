2feb61b7e4f506c3da9e448cb8e7355238606ef3
david <davidcidrod@gmail.com>
2019-02-13 14:44:04 +0100

    game-logic-done

--- game.js
+++ game.js
@@ -1,1 +1,41 @@
 'use strict'
+
+class Game{
+    constructor(canvas){
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext('2d');
+        this.player;
+        this.enemies = [];
+    };
+
+    startLoop() {
+        const loop = () =>{
+            console.log('out of loop');
+
+            this.updateCanvas();
+            this.clearCanvas();
+            this.drawCanvas();
+
+
+            window.requestAnimationFrame(loop);
+        }
+
+
+        window.requestAnimationFrame(loop);
+    };
+
+    updateCanvas() {
+
+
+    };
+    clearCanvas() {
+        this.ctx.clearRect(0,0,this.canvas.with,this.canvas.heigth);
+
+    };
+    drawCanvas() {
+
+
+    };
+
+}
+
--- main.js
+++ main.js
@@ -10,10 +10,52 @@ const main = () => {
     }
     const buildSplashScreen = () => {
         const splashScreen = buildDom(`
-        <h1>Hello</h1>`)
+        <section class="splash-screen-screen">
+        <h1>Eternal Enemies</h1>
+        <button>Start</button>
+        `)
+
+    const startButton = document.querySelector('button');
+    startButton.addEventListener('click',buildGameScreen);
+    
+    }
+    const buildGameScreen = () => {
+        const gameScreen = buildDom(`
+        <section class = "game-screen">
+        <canvas></canvas>
+        </section>    
+        `);
+
+        const width = document.querySelector('.game-screen').offsetWith;
+        const heigth = document.querySelector('.game-screen').offsetHeigth;
+
+        const canvasElement = document.querySelector('canvas');
+
+        canvasElement.setAttribute('width',width);
+        canvasElement.setAttribute('heigth',heigth);
+
+        setTimeout(buildGameOver, 3000);
+
+        const game = new Game(canvasElement);
+        game.startLoop(); 
 
     }
+    const buildGameOver = () => {
+        const gameOverScreen = buildDom(`
+        <section class = "game-over">
+        <h1>Game-Over</h1>
+        <button>Restart</button>
+        </section>
+        `)
+
+        const restartButton = document.querySelector('button');
+        restartButton.addEventListener('click',buildGameScreen);
+    }
+
+
     buildSplashScreen();
+
+
 }
 
 
